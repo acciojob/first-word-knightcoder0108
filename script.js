f0a534f77d8c1ae80edef57f31bef6aa7480daff
knightcoder0108 <ssharma.aabhi@gmail.com>
2024-06-21 01:16:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstWord(s) {
-  // your code here
+  // Trim the string to remove leading and trailing spaces
+  s = s.trim();
+
+  // Find the index of the first space in the string
+  const firstSpaceIndex = s.indexOf(' ');
+
+  // If there is no space, return the entire string
+  if (firstSpaceIndex === -1) {
+    return s;
+  }
+
+  // Return the substring from the start to the first space
+  return s.substring(0, firstSpaceIndex);
 }
 
 // Do not change the code below
